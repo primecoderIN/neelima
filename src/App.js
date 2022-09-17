5ef0212ea05f14cde51e55bd12ce77809884d36c
sanjeevk <sanjeevk@gnrgy.com>
2022-09-17 10:10:08 +0530

    Basic react rules taught

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import {Navbar} from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 import './App.css';
 
+//Rules of react and hooks 
+
+//Component name will start with capital letter only 
+
+//We can not return multiple elements from a react component
+
+//Hooks can be called only inside a functional component and that too before return
+
+//You can not import something which does not exists in your project
+
+//In react we do not use class we use className instead 
+
+//we do not use for in react we use htmlFor instead on label element
+
 function App() {
+
   return (
     <div className="App">
-       <Navbar/>
+      <Navbar />
+      <Sidebar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import "./Navbar.css"
 export const Navbar = () => {
   return (
-    <div className="container">
-      <div className="main">
+    <div class="container">
+      <div class="main">
         <h3 className="brandName">Brand Name</h3>
 
         <span className="user"> Hello Ms Neelima</span>
